06e867353257d89784975ace60e744314a772043
TomBearpark <t.bearpark@googlemail.com>
2022-03-17 11:32:56 -0400

    parallel

--- 1_bs.R
+++ 1_bs.R
@@ -1,16 +1,14 @@
 if(!require(pacman)) install.packages('pacman')
-pacman::p_load(tidyverse)
-
+pacman::p_load(tidyverse, xtable, MASS, furrr)
+theme_set(theme_bw())
+seed   <- 8894; set.seed(seed)
+ncores <- 6
 
 # set parameters ----------------------------------------------------------
-
 k <- 10
 M <- 5000
 N <- 1000
-df <- tibble(i = 1:k, 
-             theta_1 = i, 
-             theta_2 = 10 * i, 
-             theta_3 = i / 10)
+df <- tibble(i = 1:k, theta_1 = i, theta_2 = 10 * i, theta_3 = i / 10)
 
 # functions ---------------------------------------------------------------
 
@@ -20,11 +18,12 @@ draw_theta_hat <- function(theta, k){
 
 run_bs_draw <- function(m, theta_var, df, k, N)
   {
+  if(m %% 100 == 0) message(paste0("---- draw ", m, " ----"))
   # Get an initial draw of theta hat and their ranks
   theta_hat <- draw_theta_hat(df[[theta_var]], k)
   r1        <- matrix(rank(theta_hat), nrow = 1)
   
-  # Draw and rank all N normal draws of dim k. subtract r1 from each draw
+  # Draw all N normal draws of dim k. 
   norms <- matrix(nrow = N, ncol = k)
   ranks <- norms
   for(i in 1:k){
@@ -35,35 +34,42 @@ run_bs_draw <- function(m, theta_var, df, k, N)
   }
   # Get quantiles
   map_dfr(1:k, function(kk) quantile(ranks[,kk], c(0.025, 0.975))) %>% 
-    mutate(i = 1:k) %>% 
-    select(i, q.025 = `2.5%`, q.975 = `97.5%`) %>% 
-    mutate(theta = df[[theta_var]], r1 = drop(r1)) %>%
-    mutate(lower_percentile = r1 - q.975, upper_percentile = r1 - q.025, 
-           lower_effron =     r1 + q.025, upper_effron     = r1 + q.975) %>% 
+    mutate(i = 1:k, theta = df[[theta_var]], r1 = drop(r1)) %>%
+    mutate(lower_percentile = r1 - `97.5%`, upper_percentile = r1 - `2.5%`, 
+           lower_effron     = r1 + `2.5%`,  upper_effron     = r1 + `97.5%`) %>% 
     mutate(percentile_covered = 
-             ifelse(i >= lower_percentile & i <= upper_percentile, 1, 0)) %>% 
-    mutate(effron_covered = 
+             ifelse(i >= lower_percentile & i <= upper_percentile, 1, 0), 
+           effron_covered = 
              ifelse(i >= lower_effron & i <= upper_effron, 1, 0)) %>% 
-    select(i, percentile_covered, effron_covered) %>%
-    mutate(m = !!m)
+    dplyr::select(i, percentile_covered, effron_covered)
 }
 
-run_sim <- function(M, theta_var, df, k, N){
-  map_dfr(1:M, run_bs_draw, theta_var, df, k, N) %>% 
-    group_by(i) %>% summarize(percentile_covered = mean(percentile_covered), 
-                              effron_covered     = mean(effron_covered)) %>% 
+run_sim <- function(M, theta_var, df, k, N, seed = 8894){
+  future_map_dfr(1:M, run_bs_draw, theta_var, df, k, N, 
+                 .options = furrr_options(seed = seed), 
+                 .progress = TRUE) %>% 
+    group_by(i) %>% 
+    summarize(percentile_covered = mean(percentile_covered), 
+              effron_covered     = mean(effron_covered)) %>% 
     mutate(theta = !!theta_var) %>% 
     relocate(theta)
 }
 
 # run code ----------------------------------------------------------------
-out <- 
-  run_sim(M = 1000, theta_var = "theta_1", df = df, k = k, N =N) %>% 
-  bind_rows(
-    run_sim(M = 1000, theta_var = "theta_2", df = df, k = k, N =N) 
-  ) %>% 
-  bind_rows(
-    run_sim(M = 1000, theta_var = "theta_3", df = df, k = k, N =N) 
-  )
 
+plan(multisession, workers = ncores)
+
+out1 <- run_sim(M = M, theta_var = "theta_1", df = df, k = k, N = N) 
+out2 <- run_sim(M = M, theta_var = "theta_2", df = df, k = k, N = N) 
+out3 <- run_sim(M = M, theta_var = "theta_3", df = df, k = k, N = N) 
+
+out  <- bind_rows(out1, out2, out3)
+
+# plots and tables --------------------------------------------------------
 out %>% data.frame()
+
+out %>% xtable() %>% print(include.rownames=FALSE)
+
+out %>% 
+  pivot_longer(cols = c(percentile_covered, effron_covered)) %>% 
+  ggplot() + geom_point(aes(x = i, y = value, color = name)) + facet_wrap(~theta)
